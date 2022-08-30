09f5da7764959692bffc65373262a097a3e25214
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-08-30 14:32:53 +0630

    right part of HomeMainContainer and responsive method

--- src/components/HomeMainContainer.jsx
+++ src/components/HomeMainContainer.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Delivery from "../images/delivery.png";
+import HeroBg from "../images/heroBg.png";
+import I1 from "../images/i1.png";
 
-const HomeMainContainer = () => {
+const HomeContainer = () => {
 	return (
 		<section
 			className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full"
 			id="home "
 		>
+			{/* left */}
 			<div className="py-2 flex-1 flex flex-col items-start justify-center gap-6">
 				<div className="flex items-center justify-center gap-2 bg-orange-100 px-4 py-1 rounded-full">
 					<p className="text-base text-orange-500 font-semibold">
@@ -42,9 +45,35 @@ const HomeMainContainer = () => {
 				</button>
 			</div>
 
-			<div className="py-2 bg-blue-400 flex-1 "></div>
+			{/* right */}
+
+			<div className="py-2 flex-1 flex items-center relative">
+				<img
+					src={HeroBg}
+					className="ml-auto h-420 w-full lg:w-auto lg:h-650"
+					alt="hero-bg"
+				/>
+
+				<div className=" absolute inset-0 flex items-center justify-center lg:pr-32 py-4">
+					<div className="w-190 p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center ">
+						<img src={I1} className="w-40 -mt-20" alt="i1" />
+
+						<p className="text-xl font-semibold text-textColor mt-4">
+							Icecream
+						</p>
+
+						<p className="text-sm text-lighttextGray font-semibold my-3">
+							Chocloate & vanilla
+						</p>
+
+						<p className="ext-sm font-semibold text-headingColor">
+							<span className="textt-xs text-red-600">$</span> 5.25
+						</p>
+					</div>
+				</div>
+			</div>
 		</section>
 	);
 };
 
-export default HomeMainContainer;
+export default HomeContainer;
