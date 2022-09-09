6e1d2fe610242b8240186b659bf93e819ea1e657
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-09-09 21:49:39 +0630

    add the some code

--- src/utils/fetchLocalStorageData.js
+++ src/utils/fetchLocalStorageData.js
@@ -1,8 +1,17 @@
 export const fetchUser = () => {
 	const userInfo =
-		localStorage.getItem("user") !== "undified"
+		localStorage.getItem("user") !== "undefied"
 			? JSON.parse(localStorage.getItem("user"))
 			: localStorage.clear();
 
 	return userInfo;
+};
+
+export const fetchCart = () => {
+	const cartInfo =
+		localStorage.getItem("cartItems") !== "undefied"
+			? JSON.parse(localStorage.getItem("cartItems"))
+			: localStorage.clear();
+
+	return cartInfo ? cartInfo : [];
 };
